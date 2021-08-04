e1816c5833bec4575306eb3b36e426c04ee028c7
mattheusbr <matheusferreirabruno@hotmail.com>
2021-08-04 02:16:27 -0300

    Validação CEP

---
+++ Business/Valida.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ValidadorCep.Business
+{
+    public class Valida
+    {
+        public bool IsValidCep(string cep)
+        {
+            if (!Regex.IsMatch(cep, @"^[1-9][\d]{5}$"))
+                return false;
+            var cepVerdadeiro = true;
+            foreach (var i in Enumerable.Range(0, 4))
+            {
+                if (cep.Substring(i, 1) == cep.Substring(i + 2, 1))
+                    cepVerdadeiro = false;
+            }                                
+            return cepVerdadeiro;
+        }
+    }
+}
